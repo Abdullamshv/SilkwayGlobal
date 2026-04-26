064449f8d6fcc061f849f4bb660230d84ac4a73d
Abdulla <Abdullamshv>
2026-04-26 22:09:45 +0800

    feat: update package.json and vite.config.js for deployment setup

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 export default defineConfig({
+  base: '/SilkwayGlobal/',
   plugins: [
     tailwindcss(),
     ViteImageOptimizer(),
